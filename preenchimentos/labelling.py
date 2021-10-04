02f07ce5b847cdb0eafdcfa0cc44bf65a024a76f
matiasucker <matias.ucker@gmail.com>
2021-10-03 22:31:09 -0300

    finish development

--- preenchimentos/labelling.py
+++ preenchimentos/labelling.py
@@ -8,10 +8,11 @@ image = cv2.imread('resources/bolhas.png')
 
 # Transformação da imagem para escala de cinza
 image_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+imagem_tratada = image_gray.copy()
 
 # Aplicação do desfoque Gaussian Blur na imagem, ajuda a remover algumas das bordas de alta frequência,
 # com as quais não estamos preocupados e nos permite obter uma segmentação mais limpa
-image_gray_blurred = cv2.GaussianBlur(image_gray, (7, 7), 0)
+# image_gray_blurred = cv2.GaussianBlur(image_gray, (7, 7), 0)
 
 # Obtendo as dimensões da imagem, altura e largura.
 height, width = image.shape[:2]
@@ -23,64 +24,79 @@ seedpoint = [0, 0]
 for i in range(height):
 
     # Eliminação de objetos na borda esquerda da cena
-    if image_gray_blurred[i, 0] == 255:
+    if imagem_tratada[i, 0] == 255:
         seedpoint[0] = 0
         seedpoint[1] = i
-        cv2.floodFill(image, None, seedpoint, 0)
+        cv2.floodFill(imagem_tratada, None, seedpoint, 0)
 
     # Eliminação de objetos na borda direita da cena
-    if image_gray_blurred[i, width-1] == 255:
+    if imagem_tratada[i, width-1] == 255:
         seedpoint[0] = width-1
         seedpoint[1] = i
-        cv2.floodFill(image, None, seedpoint, 0)
+        cv2.floodFill(imagem_tratada, None, seedpoint, 0)
 
 # Análise das bordas superior e inferior
 for j in range(width):
 
     # Eliminação de objetos na borda superior da cena
-    if image_gray_blurred[0, j] == 255:
+    if imagem_tratada[0, j] == 255:
         seedpoint[0] = j
         seedpoint[1] = 0
-        cv2.floodFill(image, None, seedpoint, 0)
+        cv2.floodFill(imagem_tratada, None, seedpoint, 0)
 
     # Eliminação de objetos na borda inferior da cena
-    if image_gray_blurred[height-1, j] == 255:
+    if imagem_tratada[height-1, j] == 255:
         seedpoint[0] = j
         seedpoint[1] = height-1
-        cv2.floodFill(image, None, seedpoint, 0)
+        cv2.floodFill(imagem_tratada, None, seedpoint, 0)
+
+
+
+
+seedpoint[0] = 0
+seedpoint[1] = 0
+cv2.floodFill(imagem_tratada, None, seedpoint, 1)
+
+nobjects_com_buracos = 0
+for i in range(height):
+    for j in range(width):
+        if imagem_tratada[i, j] == 0:
+            if imagem_tratada[i, j -1] == 255:
+                nobjects_com_buracos += 1
+                seedpoint[0] = j-1
+                seedpoint[1] = i
+                cv2.floodFill(imagem_tratada, None, seedpoint, 100)
 
 
 
 # Identificando regiões sem buracos
+nobjects_sem_buracos = 0
+for i in range(height):
+    for j in range(width):
+        if imagem_tratada[i, j] == 255:
+            nobjects_sem_buracos += 1
+            seedpoint[0] = j
+            seedpoint[1] = i
+            cv2.floodFill(imagem_tratada, None, seedpoint, 50)
+
+# Saída do programa labeling sem aprimoração
 nobjects = 0
 for i in range(height):
     for j in range(width):
-        if image_gray_blurred[i, j] == 255:
+        if image_gray[i, j] == 255:
             nobjects += 1
             seedpoint[0] = j
             seedpoint[1] = i
-            cv2.floodFill(image, None, seedpoint, 100)
+            cv2.floodFill(image_gray, None, seedpoint, nobjects)
 
 
-# Obtendo o threshold da imagem
-# Primeiro argumento: a imagem
-# Segundo argumento: o valor de threshold, pixels com valor menor recebem 0, pixels com valor maior recebem o output (255)
-# Terceito argumento: o valor de output (255 é o valor máximo para uma imagem em escala de cinza)
-# Quarto argumento: função que inverte, pixels com valor menor do threshold recebem 255, pixels com valor maior recebem 0
-(T, threshInv) = cv2.threshold(image_gray_blurred, 200, 255, cv2.THRESH_BINARY_INV)
+cv2.imshow('Saida do programa labeling', image_gray)
+print("A figura tem " + str(nobjects) + " objetos no total")
 
-# Obtendo os contornos dos objetos e a hierarquia destes contornos
-# Primeiro argumento: A imagem
-# Segundo argumento: Recupera todos os contornos e os organiza em uma hierarquia de dois níveis
-(contours, hierarchy) = cv2.findContours(threshInv, cv2.RETR_CCOMP, cv2.CHAIN_APPROX_SIMPLE)
+cv2.imshow('Saida do programa labeling aprimorado', imagem_tratada)
+print("A figura tem " + str(nobjects_sem_buracos) + " objetos sem buracos que não tocam as bordas da imagem")
+print("A figura tem " + str(nobjects_com_buracos) + " objetos com buracos que não tocam as bordas da imagem")
 
-
-print("A figura tem " + str(nobjects) + " objetos sem buracos")
-
-
-cv2.imshow('gray',image)
-
-print(image.shape)
 
 cv2.waitKey(0)
 
