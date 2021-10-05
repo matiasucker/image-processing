0d3cc87b5363cab598bcfb47c044635a4e6291ef
matiasucker <matias.ucker@gmail.com>
2021-10-04 23:21:10 -0300

    Initial commit

---
+++ histogramas/motion_detector.py
@@ -0,0 +1,71 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import argparse
+import cv2
+import time
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-c', '--color', type=str, default='gray',
+    help='Color space: "gray" (default), "rgb"')
+parser.add_argument('-b', '--bins', type=int, default=256,
+    help='Number of bins per channel (default 256)')
+args = vars(parser.parse_args())
+
+color = args['color']
+bins = args['bins']
+
+fig, ax = plt.subplots()
+if color == 'rgb':
+    ax.set_title('Histograma RGB')
+else:
+    ax.set_title('Histograma escala de cinza')
+ax.set_xlabel('Bins')
+ax.set_ylabel('Frequency')
+
+lw = 3
+alpha = 0.5
+if color == 'rgb':
+    lineR, = ax.plot(np.arange(bins), np.zeros((bins,)), c='r', lw=lw, alpha=alpha, label='Red')
+    lineG, = ax.plot(np.arange(bins), np.zeros((bins,)), c='g', lw=lw, alpha=alpha, label='Green')
+    lineB, = ax.plot(np.arange(bins), np.zeros((bins,)), c='b', lw=lw, alpha=alpha, label='Blue')
+else:
+    lineGray, = ax.plot(np.arange(bins), np.zeros((bins,1)), c='k', lw=lw, label='intensity')
+
+ax.set_xlim(0, bins)
+ax.set_ylim(0, 0.03)
+ax.legend()
+plt.ion()
+plt.show()
+
+capture = cv2.VideoCapture(0)
+while True:
+    (grabbed, frame) = capture.read()
+    frame = cv2.flip(frame, 0)
+    if not grabbed:
+        break
+
+    numPixels = np.prod(frame.shape[:2])
+    if color == 'rgb':
+        cv2.imshow('RGB', frame)
+        (b, g, r) = cv2.split(frame)
+        histogramR = cv2.calcHist([r], [0], None, [bins], [0, 255]) / numPixels
+        histogramG = cv2.calcHist([g], [0], None, [bins], [0, 255]) / numPixels
+        histogramB = cv2.calcHist([b], [0], None, [bins], [0, 255]) / numPixels
+        lineR.set_ydata(histogramR)
+        lineG.set_ydata(histogramG)
+        lineB.set_ydata(histogramB)
+    else:
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        cv2.imshow('Grayscale', gray)
+        histogram = cv2.calcHist([gray], [0], None, [bins], [0, 255]) / numPixels
+        lineGray.set_ydata(histogram)
+
+    fig.canvas.draw()
+    fig.canvas.flush_events()
+    time.sleep(0.1)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+capture.release()
+cv2.destroyAllWindows()
