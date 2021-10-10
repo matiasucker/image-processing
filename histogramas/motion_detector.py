b4ad581c6f5d46a8a3fed19c496a27e335778188
matiasucker <matias.ucker@gmail.com>
2021-10-10 19:53:06 -0300

    project completed

--- histogramas/motion_detector.py
+++ histogramas/motion_detector.py
@@ -1,8 +1,10 @@
+import cv2
 import numpy as np
 import matplotlib.pyplot as plt
+from datetime import datetime
 import argparse
-import cv2
 import time
+
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-c', '--color', type=str, default='gray',
@@ -13,6 +15,7 @@ args = vars(parser.parse_args())
 
 color = args['color']
 bins = args['bins']
+
 
 fig, ax = plt.subplots()
 if color == 'rgb':
@@ -32,28 +35,70 @@ else:
     lineGray, = ax.plot(np.arange(bins), np.zeros((bins,1)), c='k', lw=lw, label='intensity')
 
 ax.set_xlim(0, bins)
-ax.set_ylim(0, 0.03)
+ax.set_ylim(0, bins + 50)
 ax.legend()
 plt.ion()
 plt.show()
 
+
+image_referenceR = np.zeros((480, 640), dtype="uint8")
+histogram_referenceR = cv2.calcHist([image_referenceR], [0], None, [bins], [0, 255])
+
+image_referenceG = np.zeros((480, 640), dtype="uint8")
+histogram_referenceG = cv2.calcHist([image_referenceG], [0], None, [bins], [0, 255])
+
+image_referenceB = np.zeros((480, 640), dtype="uint8")
+histogram_referenceB = cv2.calcHist([image_referenceB], [0], None, [bins], [0, 255])
+
 capture = cv2.VideoCapture(0)
 while True:
-    (grabbed, frame) = capture.read()
+    existe_frame, frame = capture.read()
     frame = cv2.flip(frame, 0)
-    if not grabbed:
+
+    if not existe_frame:
         break
 
     numPixels = np.prod(frame.shape[:2])
     if color == 'rgb':
         cv2.imshow('RGB', frame)
         (b, g, r) = cv2.split(frame)
-        histogramR = cv2.calcHist([r], [0], None, [bins], [0, 255]) / numPixels
-        histogramG = cv2.calcHist([g], [0], None, [bins], [0, 255]) / numPixels
-        histogramB = cv2.calcHist([b], [0], None, [bins], [0, 255]) / numPixels
+        histogramR = cv2.calcHist([r], [0], None, [bins], [0, 255])
+        histogramG = cv2.calcHist([g], [0], None, [bins], [0, 255])
+        histogramB = cv2.calcHist([b], [0], None, [bins], [0, 255])
+
+        cv2.normalize(histogramR, histogramR, 0, 255, cv2.NORM_MINMAX)
+        cv2.normalize(histogramG, histogramG, 0, 255, cv2.NORM_MINMAX)
+        cv2.normalize(histogramB, histogramB, 0, 255, cv2.NORM_MINMAX)
+
         lineR.set_ydata(histogramR)
         lineG.set_ydata(histogramG)
         lineB.set_ydata(histogramB)
+
+        similarityR = cv2.compareHist(histogram_referenceR, histogramR, cv2.HISTCMP_CORREL)
+        similarityG = cv2.compareHist(histogram_referenceG, histogramG, cv2.HISTCMP_CORREL)
+        similarityB = cv2.compareHist(histogram_referenceB, histogramB, cv2.HISTCMP_CORREL)
+
+        similarity = (similarityR + similarityG + similarityB) / 3
+        print(similarity)
+
+        if similarityR < 0.99 or similarityG < 0.99 or similarityB < 0.99:
+            print("[INFO] ALARME: movimento detectado " + datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
+            #print(similarityR)
+            #print(similarityG)
+            #print(similarityB)
+
+        #print(similarityR)
+        #print(similarityG)
+        #print(similarityB)
+
+        histogram_referenceR = histogramR.copy()
+        histogram_referenceG = histogramG.copy()
+        histogram_referenceB = histogramB.copy()
+
+
+
+
+        #print(a)
     else:
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         cv2.imshow('Grayscale', gray)
