dd31800ecc570427f86cb8c872f9aa08003129f8
matiasucker <matias.ucker@gmail.com>
2021-11-22 22:59:54 -0300

    Update kmeans

--- k-means/kmeans.py
+++ k-means/kmeans.py
@@ -10,11 +10,12 @@ samples = image.reshape((-1, 3))
 samples = np.float32(samples)
 
 ret, labels, centers = cv2.kmeans(samples,
-                                      NCLUSTERS,
-                                      None,
-                                      (cv2.TERM_CRITERIA_MAX_ITER | cv2.TERM_CRITERIA_EPS, 10000, 0.0001),
-                                      NROUNDS,
-                                      cv2.KMEANS_RANDOM_CENTERS)
+                                  NCLUSTERS,
+                                  None,
+                                  (cv2.TERM_CRITERIA_MAX_ITER | cv2.TERM_CRITERIA_EPS, 10000, 0.0001),
+                                  NROUNDS,
+                                  cv2.KMEANS_RANDOM_CENTERS)
+
 centers = np.uint8(centers)
 res = centers[labels.flatten()]
 res2 = res.reshape((image.shape))
