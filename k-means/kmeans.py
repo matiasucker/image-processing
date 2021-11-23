5335db8afaab433aa09bec30747b976010923f7b
matiasucker <matias.ucker@gmail.com>
2021-11-22 22:54:20 -0300

    Update kmeans

--- k-means/kmeans.py
+++ k-means/kmeans.py
@@ -4,33 +4,22 @@ import numpy as np
 NCLUSTERS = 8
 NROUNDS = 1
 
-image = cv2.imread("resources/img4.png", cv2.IMREAD_COLOR)
-height, width, channels = image.shape
-samples = np.zeros([height * width, 3], dtype=np.float32)
-count = 0
+image = cv2.imread("resources/img5.png", cv2.IMREAD_COLOR)
 
-for x in range(height):
-    for y in range(width):
-        samples[count] = image[x][y]
-        count += 1
+samples = image.reshape((-1, 3))
+samples = np.float32(samples)
 
-compact, labels, centers = cv2.kmeans(samples,
+ret, labels, centers = cv2.kmeans(samples,
                                       NCLUSTERS,
                                       None,
                                       (cv2.TERM_CRITERIA_MAX_ITER | cv2.TERM_CRITERIA_EPS, 10000, 0.0001),
                                       NROUNDS,
                                       cv2.KMEANS_RANDOM_CENTERS)
-print(centers)
 centers = np.uint8(centers)
-print(centers)
-print(labels)
 res = centers[labels.flatten()]
-print(res)
-image2 = res.reshape(image.shape)
-print(image2)
+res2 = res.reshape((image.shape))
 
-cv2.imshow("k-means", image2)
-cv2.imwrite("output/k-means.jpg", image2)
-
+cv2.imshow("k-means", res2)
+cv2.imwrite("output/k-means.jpg", res2)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
