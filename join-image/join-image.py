80c90f27a9bcffd00d7cb0b59a1bc264047692db
matiasucker <matias.ucker@gmail.com>
2021-12-09 10:52:40 -0300

    remove join-image folder

--- join-image/join-image.py
+++
@@ -1,8 +0,0 @@
-import cv2
-
-ideal = cv2.imread("resources/ideal.png", cv2.IMREAD_COLOR)
-real = cv2.imread("resources/real.png", cv2.IMREAD_COLOR)
-
-resultado = cv2.addWeighted(real, 0.5, ideal, 0.5, 0.0)
-
-cv2.imwrite("output/Resultado.png", resultado)
